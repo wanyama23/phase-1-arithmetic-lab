eb4865c78c710c81ca25310769bfc7f8e9dd89a4
edmond <edmond.wanyama@student.moringaschool.com>
2023-06-13 16:24:30 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
 // Write your code here
+let multiply
+let num1=2
+let num2=31
+
+multiply = num1* num2
+
+let random;
+random=Math.floor(Math.random() *10) +1;
+
+let mod;
+let num3=24;
+let num4=5;
+
+mod=num3%num4;
+
+let max=Math.max(5,17,20,15);
+
